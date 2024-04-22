17b1f7e31e0d1e9d1925f53582040ab5d39dcb85
monank sojitra <monaksojitra4444@gmail.com>
2024-04-22 21:46:31 +0530

    [add]:vercel.json

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import app from "./utils/app.js";
 connectDB();
 // app.use("/", getProperty);
 app.use("/api/v1/auth", authRoutes);
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
 app.use("/api/v1/post", [authorizeBearerToken], postRoutes);
 
 const PORT = process.env.PORT || 5000;
