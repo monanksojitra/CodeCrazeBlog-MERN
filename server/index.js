5cdb5b9bb3c02d70218b4beedf63bf8125b8e0a8
monank sojitra <monaksojitra4444@gmail.com>
2024-04-22 21:18:16 +0530

    [setup]:env

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
-
 import { authorizeBearerToken } from "./middlewares/jsonwebtoken.js";
 import connectDB from "./models/Db.js";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/post.js";
 import app from "./utils/app.js";
 
-
 connectDB();
 // app.use("/", getProperty);
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/post", authorizeBearerToken, postRoutes);
+app.use("/api/v1/post", [authorizeBearerToken], postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
